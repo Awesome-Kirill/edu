6162eb538cd1cd33912d568610860eeb681e4e59
Awesome-Kirill <ka.stulnikov@gmail.com>
2017-06-25 21:04:25 +0300

    a

---
+++ stepik/test_0.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+
+
+while True:
+	input_string = input("")
+	if input_string == 'End':
+		print('Good bye!')
+		break
+	print('Processing "{}" command...'.format(input_string))
